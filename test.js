fe4e93551a8bffc03864de042ed4c5cbc0ca40f4
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-12 15:16:17 -0600

    initial commit

---
+++ test.js
@@ -0,0 +1,39 @@
+{
+name: 'Galvanize Heights',
+address: {
+  line1: '345 1st Avenue',
+  line2: 'Denver, CO'
+},
+units: [{
+  number: 1,
+  tenants: [{
+    first: 'Joe',
+    last: 'Smith',
+    lease: {
+      start: '2016-01-01',
+      end: '2016-12-31'
+      }
+    }]
+  }]
+}
+
+
+{'  name: 'Denver Heights',
+  address: {'    line1: '748 First St',
+    city: 'Broomfield',
+    state: 'CO',
+    zip: 80210
+  },
+  units: [{
+    price: 130000,
+    number: 11,
+    tenants: [{
+      firstName: 'Steve',
+      lastName: 'Box',
+      lease: [{
+        start: new Date('2016-01-01'),
+        end: new Date('2017-01-01')
+      }]
+    }]
+  }]
+}
